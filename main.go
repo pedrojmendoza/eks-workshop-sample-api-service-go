5c5550e89fed2b15035fcbc96b2d0edeedf6061a
pedrojmendoza <pedrojmendoza@gmail.com>
2019-05-13 16:33:38 +0200

    Msg updated to include HHMM -> 1633

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World on 20190513"}
+		res := &response{Message: "Hello World on 20190513 1633"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
